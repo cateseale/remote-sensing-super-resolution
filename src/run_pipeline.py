5662c8a97bad6adbd090a06ad3fb873c83a9f8f0
cate <catherineseale@gmail.com>
2020-01-07 11:56:36 +0000

    add option ot use different models as the loss function

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -3,11 +3,11 @@
 from preprocessing import load_images
 from train import train
 
-def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size):
+def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
 
     X_train, X_val, X_test, y_train, y_val, y_test = load_images(path_to_data_folder)
 
-    train(X_train, X_test, y_train, y_test, low_res_shape, high_res_shape, epochs, batch_size, path_to_data_folder)
+    train(X_train, X_test, y_train, y_test, low_res_shape, high_res_shape, epochs, batch_size, path_to_data_folder, loss_model)
 
 
 
@@ -19,4 +19,5 @@ if __name__== "__main__":
     hr_shape = (256, 256, 3)
     data_dir = '/Users/cate/Documents/Data_Science_MSc/ECMM433/data/'
 
-    run(data_dir, lr_shape, hr_shape, epochs, batch_size)
+    run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='vgg')
+    # run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='mangrove')
