dd1e5f887eb6255859c8d5026603a5649a76e3a6
cate <catherineseale@gmail.com>
2020-01-08 09:36:31 +0000

    let mangrove loss be used without error by disabling eager execution

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -3,7 +3,14 @@
 from preprocessing import load_images
 from train import train
 
+from tensorflow.python.framework.ops import disable_eager_execution
+
+
+
 def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
+
+    if loss_model == 'mangrove':
+        disable_eager_execution()
 
     X_train, X_val, X_test, y_train, y_val, y_test = load_images(path_to_data_folder)
 
@@ -19,5 +26,5 @@ if __name__== "__main__":
     hr_shape = (256, 256, 3)
     data_dir = '/Users/cate/Documents/Data_Science_MSc/ECMM433/data/'
 
-    run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='vgg')
-    # run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='mangrove')
+    # run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='vgg')
+    run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='mangrove')
