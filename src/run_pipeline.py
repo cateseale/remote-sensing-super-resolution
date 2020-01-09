5b66fe902870c0a5ab0a8bf74d6e5cb7c2357f6b
cate <catherineseale@gmail.com>
2020-01-09 15:50:39 +0000

    and mlflow logging

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from preprocessing import load_images
-from train import train
-from utils import save_train_test_split
 
+from train import train
+from preprocessing import load_images
+from utils import save_train_test_split
 from tensorflow.python.framework.ops import disable_eager_execution
+from mlflow import log_metric, log_param, log_artifact
+import mlflow
+
 
 
 def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
+
+    log_param("loss_model", loss_model)
 
     X_train, X_val, X_test, y_train, y_val, y_test = load_images(path_to_data_folder)
 
@@ -26,12 +31,17 @@ def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, 
 if __name__== "__main__":
 
     epochs = 2
-    batch_size = 20
+    batch_size = 28
     lr_shape = (64, 64, 3)
     hr_shape = (256, 256, 3)
     data_dir = '/Users/cate/Documents/Data_Science_MSc/ECMM433/data'
-    gen = 'gen_model1.h5'
-    dis = 'dis_model1.h5'
+
+
+    log_param("epochs", epochs)
+    log_param("batch_size", batch_size)
+    log_param("lr_shape", lr_shape)
+    log_param("hr_shape", hr_shape)
+    log_param("data_dir", data_dir)
 
     run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='vgg')
     # run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='mangrove')
