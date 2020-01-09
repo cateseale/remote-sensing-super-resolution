ca7b273f6e15b2dab1fc3460c7f982e98aa2f4dd
cate <catherineseale@gmail.com>
2020-01-09 15:51:08 +0000

    add mlflow logging

--- src/train.py
+++ src/train.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 from tensorflow import keras
 from model import VggLoss, get_optimizer, Generator, Discriminator
 from mangrove_model import MangroveLoss
+from mlflow import log_metric, log_param, log_artifact
 
 
 # Combined network
@@ -27,6 +28,9 @@ def train(X_train, X_test, y_train, y_test, input_shape, output_shape, epochs, b
 
     model_save_dir = os.path.join(data_dir, 'models')
     output_dir = os.path.join(data_dir, 'results')
+
+    log_param("model_save_dir", model_save_dir)
+    log_param("output_dir", output_dir)
 
     if loss_model == 'vgg':
         print ('Using vgg loss')
@@ -79,15 +83,26 @@ def train(X_train, X_test, y_train, y_test, input_shape, output_shape, epochs, b
             print("discriminator_loss : %f" % discriminator_loss)
             print("gan_loss :", gan_loss)
 
+            # Log a metric; metrics can be updated throughout the run
+            log_metric("gan_perceptual_loss", gan_loss[0])
+            log_metric("gan_content_loss", gan_loss[1])
+            log_metric("gan_adversarial_loss", gan_loss[2])
+            log_metric("discriminator_loss", discriminator_loss)
+
             gan_loss = str(gan_loss)
 
-            loss_file = open(os.path.join(model_save_dir, 'losses.txt'), 'a')
-            loss_file.write('epoch%d : gan_loss = %s ; discriminator_loss = %f\n' % (e, gan_loss, discriminator_loss))
-            loss_file.close()
+            # loss_file = open(os.path.join(model_save_dir, 'losses.txt'), 'a')
+            # loss_file.write('epoch%d : gan_loss = %s ; discriminator_loss = %f\n' % (e, gan_loss, discriminator_loss))
+            # loss_file.close()
+
+            loss_output = os.path.join(model_save_dir, 'losses.txt')
+            with open(loss_output, "w") as f:
+                f.write('epoch%d : gan_loss = %s ; discriminator_loss = %f\n' % (e, gan_loss, discriminator_loss))
+            log_artifact(loss_output)
 
         if e == 1 or e % 5 == 0:
             # plot_generated_images(output_dir, e, generator, y_test, X_test)
             save_images(X_test, y_test, generator, path=os.path.join(output_dir, "img_epoch{}".format(e)))
-        if e % 1 == 0:
+        if e % 200 == 0:
             generator.save(os.path.join(model_save_dir, 'gen_model%d.h5' % e))
             discriminator.save(os.path.join(model_save_dir, 'dis_model%d.h5' % e))
