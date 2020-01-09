45b3eb921e4653cf49dc61abe985d576881768d5
cate <catherineseale@gmail.com>
2020-01-09 10:53:19 +0000

    save the train test split that is created

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -2,22 +2,19 @@
 
 from preprocessing import load_images
 from train import train
+from utils import save_train_test_split
 
 from tensorflow.python.framework.ops import disable_eager_execution
 
 
-def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg', continue_learning=False,
-        pretrained_gen='', pretrained_dis=''):
+def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
 
     X_train, X_val, X_test, y_train, y_val, y_test = load_images(path_to_data_folder)
 
+    save_train_test_split([X_train, X_val, X_test, y_train, y_val, y_test], data_dir)
+
     if loss_model == 'mangrove':
         disable_eager_execution()
-
-    if continue_learning:
-        print('Using pretrained models {} and {}'.format(pretrained_gen, pretrained_dis))
-        train(X_train, X_val, y_train, y_val, low_res_shape, high_res_shape, epochs, batch_size, path_to_data_folder,
-              loss_model, pretrained_gen, pretrained_dis)
 
     else:
         print('Training new model')
@@ -36,6 +33,5 @@ if __name__== "__main__":
     gen = 'gen_model1.h5'
     dis = 'dis_model1.h5'
 
-    run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='vgg', continue_learning=False,
-        pretrained_gen=gen, pretrained_dis=dis)
+    run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='vgg')
     # run(data_dir, lr_shape, hr_shape, epochs, batch_size, loss_model='mangrove')
