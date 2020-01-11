c186a18339f906cd5ed79ed6bba764a41e63a0a5
cate <catherineseale@gmail.com>
2020-01-11 13:20:13 +0000

    add function to save test data only

--- src/utils.py
+++ src/utils.py
@@ -102,5 +102,18 @@ def save_train_test_split(image_splits_list, save_dir):
     print('Train/val/test image split created at {}'.format(data_created_timestamp))
 
 
+def save_test_data(test_list, save_dir):
+
+    timestamp_obj = datetime.now()
+    data_created_timestamp = timestamp_obj.strftime('%Y%m%d_%H%M%S')
+
+    filenames = [os.path.join(save_dir, 'images', data_created_timestamp + '_X_test'),
+                 os.path.join(save_dir, 'images', data_created_timestamp + '_y_test')]
+
+    for i in range(0,2):
+        np.save(filenames[i], test_list[i])
+
+    print('Test data created at {}'.format(data_created_timestamp))
+
 
 
