1b23f0e1c3cbfac785c1fff7113795bb241fa799
cate <catherineseale@gmail.com>
2020-01-13 14:41:40 +0000

    disable eager execution

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -10,19 +10,21 @@ from mlflow import log_metric, log_param, log_artifact
 
 def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
 
-    tf.keras.backend.clear_session()
+    disable_eager_execution()
 
-    gpus = tf.config.experimental.list_physical_devices('GPU')
-    if gpus:
-        try:
-            # Currently, memory growth needs to be the same across GPUs
-            for gpu in gpus:
-                tf.config.experimental.set_memory_growth(gpu, True)
-            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-        except RuntimeError as e:
-            # Memory growth must be set before GPUs have been initialized
-            print(e)
+    # tf.keras.backend.clear_session()
+    #
+    # gpus = tf.config.experimental.list_physical_devices('GPU')
+    # if gpus:
+    #     try:
+    #         # Currently, memory growth needs to be the same across GPUs
+    #         for gpu in gpus:
+    #             tf.config.experimental.set_memory_growth(gpu, True)
+    #         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    #         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    #     except RuntimeError as e:
+    #         # Memory growth must be set before GPUs have been initialized
+    #         print(e)
 
     log_param("loss_model", loss_model)
 
@@ -48,7 +50,7 @@ def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, 
 if __name__== "__main__":
 
     epochs = 1
-    batch_size = 2
+    batch_size = 1
     lr_shape = (64, 64, 3)
     hr_shape = (256, 256, 3)
     # data_dir = '/Users/cate/data/gans'
