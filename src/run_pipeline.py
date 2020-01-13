db03f6f63096f9c21ef145c40d65a0db557537a7
cate <catherineseale@gmail.com>
2020-01-13 14:52:29 +0000

    remove old code

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -12,36 +12,12 @@ def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, 
 
     disable_eager_execution()
 
-    # tf.keras.backend.clear_session()
-    #
-    # gpus = tf.config.experimental.list_physical_devices('GPU')
-    # if gpus:
-    #     try:
-    #         # Currently, memory growth needs to be the same across GPUs
-    #         for gpu in gpus:
-    #             tf.config.experimental.set_memory_growth(gpu, True)
-    #         logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    #         print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-    #     except RuntimeError as e:
-    #         # Memory growth must be set before GPUs have been initialized
-    #         print(e)
-
     log_param("loss_model", loss_model)
 
     low_res_imgs_paths, high_res_imgs_paths = list_images(os.path.join(path_to_data_folder))
 
-    # X_train, X_val, X_test, y_train, y_val, y_test = load_images(path_to_data_folder)
-    #
-    # save_test_data([X_test, y_test], data_dir)
-    #
-    # print('Test data saved')
-    #
-    if loss_model == 'mangrove':
-        disable_eager_execution()
 
-    else:
-        print('Training new model')
-        train(low_res_imgs_paths, high_res_imgs_paths, low_res_shape, high_res_shape, batch_size, epochs, data_dir,
+    train(low_res_imgs_paths, high_res_imgs_paths, low_res_shape, high_res_shape, batch_size, epochs, data_dir,
               loss_model=loss_model, workers=1)
 
     print ('Pipeline complete.')
@@ -50,7 +26,7 @@ def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, 
 if __name__== "__main__":
 
     epochs = 1
-    batch_size = 1
+    batch_size = 64
     lr_shape = (64, 64, 3)
     hr_shape = (256, 256, 3)
     # data_dir = '/Users/cate/data/gans'
