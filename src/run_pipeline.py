d23baf80f4e3e01ea56106c1cd5da2919e9b4a30
cate <catherineseale@gmail.com>
2020-01-13 13:34:07 +0000

    limit GPU memory growth

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 
 import os
+import tensorflow as tf
 from train import train
 from preprocessing import list_images
-from utils import save_test_data
 from tensorflow.python.framework.ops import disable_eager_execution
 from mlflow import log_metric, log_param, log_artifact
 
 
 def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
+
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
     log_param("loss_model", loss_model)
 
