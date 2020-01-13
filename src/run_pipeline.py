9cbf03e9ab2c0a66da63bd694ae0560a4b28a882
cate <catherineseale@gmail.com>
2020-01-13 14:11:25 +0000

    testing tf gpu memory options

--- src/run_pipeline.py
+++ src/run_pipeline.py
@@ -10,18 +10,16 @@ from mlflow import log_metric, log_param, log_artifact
 
 def run(path_to_data_folder, low_res_shape, high_res_shape, epochs, batch_size, loss_model='vgg'):
 
-
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
-        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
         try:
-            tf.config.experimental.set_virtual_device_configuration(
-                gpus[0],
-                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
-            # Virtual devices must be set before GPUs have been initialized
+            # Memory growth must be set before GPUs have been initialized
             print(e)
 
     log_param("loss_model", loss_model)
