85507419d19b753a7e9b7aef22d35575cfcce87c
cate <catherineseale@gmail.com>
2020-01-24 14:46:45 +0000

    add save image function

--- SRGAN/utils.py
+++ SRGAN/utils.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import imageio
 from PIL import Image
+import os
 
 
 def load_image(path):
     return np.array(Image.open(path))
 
+
+def save_image(array, out_dir, folder, im_id):
+    imageio.imwrite(os.path.join(out_dir, folder, 'img_' + im_id + '.png'), array)
 
 def plot_sample(lr, sr):
     plt.figure(figsize=(20, 10))
