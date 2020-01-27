0ef64548dfd787eef68eb94754eb03d4101a1571
cate <catherineseale@gmail.com>
2020-01-27 21:58:33 +0000

    add code for training road classifier

---
+++ unet/unet_run_pipeline.py
@@ -0,0 +1,153 @@
+import tensorflow as tf
+import numpy as np
+import os
+from tensorflow.python.keras.layers import Add, concatenate, BatchNormalization, Dropout, Conv2DTranspose, MaxPooling2D, Conv2D, Dense, Flatten, Input, LeakyReLU, PReLU, Lambda
+from tensorflow.python.keras.models import Model
+import pickle
+
+
+def load_data(x_train, y_train, x_val, y_val):
+
+    y_train = np.expand_dims(y_train, 3)
+    y_val = np.expand_dims(y_val, 3)
+
+    X_train = x_train.astype('float32')
+    X_val = x_val.astype('float32')
+
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    val_ds = tf.data.Dataset.from_tensor_slices((X_val, y_val))
+
+    return train_ds, val_ds
+
+
+def unet(HR_SIZE=256):
+# Build U-Net model
+    inputs = Input((HR_SIZE, HR_SIZE, 3))
+#     s = Lambda(lambda x: x / 255) (inputs)
+
+    c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (inputs)
+    c1 = BatchNormalization()(c1)
+    c1 = Dropout(0.1) (c1)
+    c1 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c1)
+    c1 = BatchNormalization()(c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
+
+    c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p1)
+    c2 = BatchNormalization()(c2)
+    c2 = Dropout(0.1) (c2)
+    c2 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c2)
+    c2 = BatchNormalization()(c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
+
+    c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p2)
+    c3 = BatchNormalization()(c3)
+    c3 = Dropout(0.2) (c3)
+    c3 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c3)
+    c3 = BatchNormalization()(c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+
+    c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p3)
+    c4 = BatchNormalization()(c4)
+    c4 = Dropout(0.2) (c4)
+    c4 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c4)
+    c4 = BatchNormalization()(c4)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (p4)
+    c5 = BatchNormalization()(c5)
+    c5 = Dropout(0.3) (c5)
+    c5 = Conv2D(512, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c5)
+    c5 = BatchNormalization()(c5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u6)
+    c6 = BatchNormalization()(c6)
+    c6 = Dropout(0.2) (c6)
+    c6 = Conv2D(256, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c6)
+    c6 = BatchNormalization()(c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u7)
+    c7 = BatchNormalization()(c7)
+    c7 = Dropout(0.2) (c7)
+    c7 = Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c7)
+    c7 = BatchNormalization()(c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u8)
+    c8 = BatchNormalization()(c8)
+    c8 = Dropout(0.1) (c8)
+    c8 = Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c8)
+    c8 = BatchNormalization()(c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (u9)
+    c9 = BatchNormalization()(c9)
+    c9 = Dropout(0.1) (c9)
+    c9 = Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same') (c9)
+    c9 = BatchNormalization()(c9)
+
+    outputs = Conv2D(2, (1, 1), activation='softmax') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+
+    return model
+
+
+def normalize(input_image, input_mask):
+    input_image = tf.cast(input_image, tf.float32) / 255.0
+    return input_image, input_mask
+
+
+def train(train_ds, val_ds, EPOCHS=5):
+
+    TRAIN_LENGTH = len(list(train_ds))
+    BATCH_SIZE = 64
+    BUFFER_SIZE = 100
+    STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+
+    train_ds_normalised = train_ds.map(normalize)
+
+    train_dataset = train_ds_normalised.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+    train_dataset = train_dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    test_dataset = val_ds.batch(BATCH_SIZE)
+
+    VAL_SUBSPLITS = 5
+    VALIDATION_STEPS = len(list(val_ds)) // BATCH_SIZE // VAL_SUBSPLITS
+
+    model = unet()
+    model.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.sparse_categorical_crossentropy,
+                  metrics=['accuracy'])
+
+    model_history = model.fit(train_dataset, epochs=EPOCHS,
+                              steps_per_epoch=STEPS_PER_EPOCH,
+                              validation_steps=VALIDATION_STEPS,
+                              validation_data=test_dataset)
+
+    return model_history, model
+
+
+
+if __name__ == "__main__":
+
+    # data_dir = '/home/ec2-user/gan/data/houston'
+    data_dir = '/Users/cate/data/gans/data_prep/houston'
+
+
+    X_train = np.load(os.path.join(data_dir, 'train', 'houston_Xtrain.npy'))
+    y_train = np.load(os.path.join(data_dir, 'train', 'houston_ytrain.npy'))
+    X_val = np.load(os.path.join(data_dir, 'val', 'houston_Xval.npy'))
+    y_val = np.load(os.path.join(data_dir, 'val', 'houston_yval.npy'))
+
+    train_dataset, val_dataset = load_data(X_train, y_train, X_val, y_val)
+
+    history, trained_model = train(train_dataset, val_dataset, EPOCHS=2)
+
+    trained_model.save(os.path.join(data_dir, 'models', 'cnn_model.h5'))
+
+    with open(os.path.join(data_dir, 'results', 'trainHistoryDict', 'wb')) as file_pi:
+        pickle.dump(history.history, file_pi)
