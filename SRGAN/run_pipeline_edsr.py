14b2d4595d63a99704f552d980a6abadd0b2824d
cate <catherineseale@gmail.com>
2020-02-15 16:24:30 +0000

    run only the fine tuning

--- SRGAN/run_pipeline_edsr.py
+++ SRGAN/run_pipeline_edsr.py
@@ -32,27 +32,27 @@ if __name__ == "__main__":
     train_ds = catesr_train.dataset(batch_size=16, random_transform=True, shuffle_buffer_size=500)
     valid_ds = catesr_valid.dataset(batch_size=1, random_transform=False, repeat_count=1)
 
-    trainer = EdsrTrainer(model=edsr(scale=scale, num_res_blocks=depth), checkpoint_dir=f'.ckpt/edsr-{depth}-x{scale}')
-
-    # Train EDSR model for 300,000 steps and evaluate model
-    # every 1000 steps on the first 10 images of the DIV2K
-    # validation set. Save a checkpoint only if evaluation
-    # PSNR has improved.
-    trainer.train(train_ds,
-                  valid_ds.take(20),
-                  steps=300000,
-                  evaluate_every=1000,
-                  save_best_only=True)
-
-    # Restore from checkpoint with highest PSNR
-    trainer.restore()
-
-    # Evaluate model on full validation set
-    psnrv = trainer.evaluate(valid_ds)
-    print(f'PSNR = {psnrv.numpy():3f}')
-
-    # Save weights to separate location (needed for demo)
-    trainer.model.save_weights(weights_file)
+    # trainer = EdsrTrainer(model=edsr(scale=scale, num_res_blocks=depth), checkpoint_dir=f'.ckpt/edsr-{depth}-x{scale}')
+    #
+    # # Train EDSR model for 300,000 steps and evaluate model
+    # # every 1000 steps on the first 10 images of the DIV2K
+    # # validation set. Save a checkpoint only if evaluation
+    # # PSNR has improved.
+    # trainer.train(train_ds,
+    #               valid_ds.take(20),
+    #               steps=300000,
+    #               evaluate_every=1000,
+    #               save_best_only=True)
+    #
+    # # Restore from checkpoint with highest PSNR
+    # trainer.restore()
+    #
+    # # Evaluate model on full validation set
+    # psnrv = trainer.evaluate(valid_ds)
+    # print(f'PSNR = {psnrv.numpy():3f}')
+    #
+    # # Save weights to separate location (needed for demo)
+    # trainer.model.save_weights(weights_file)
 
     # Create EDSR generator and init with pre-trained weights
     generator = edsr(scale=4, num_res_blocks=16)
