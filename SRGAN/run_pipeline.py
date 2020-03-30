c35d24105104ad7932b24e308b5f0a98a205b1bb
cate <catherineseale@gmail.com>
2020-03-30 10:57:04 +0100

    fix file paths

--- SRGAN/run_pipeline.py
+++ SRGAN/run_pipeline.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
 
 
     catesr_train = CATESR(subset='train', images_dir='/home/ec2-user/gan/data/images_rgb',
-                          caches_dir='/home/ec2-user/gan/data/caches_rgb')
+                          caches_dir='/home/ec2-user/gans/data/caches_rgb')
     catesr_valid = CATESR(subset='valid', images_dir='/home/ec2-user/gan/data/images_rgb',
-                          caches_dir='/home/ec2-user/gan/data/caches_rgb')
+                          caches_dir='/home/ec2-user/gans/data/caches_rgb')
 
 
     train_ds = catesr_train.dataset(batch_size=16, random_transform=True, shuffle_buffer_size=500)
