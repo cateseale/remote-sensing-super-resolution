05b0cb868d8f548a79e9c976134b878bbfc67853
cate <catherineseale@gmail.com>
2020-03-31 07:59:38 +0100

    load pretrained weights for edsr

--- SRGAN/run_pipeline_edsr.py
+++ SRGAN/run_pipeline_edsr.py
@@ -23,36 +23,39 @@ if __name__ == "__main__":
     #                       caches_dir='/Users/cate/data/gans/caches_rgb')
 
 
-    catesr_train = CATESR(subset='train', images_dir='/home/ec2-user/gan/data/images_rgb',
-                          caches_dir='/home/ec2-user/gan/data/caches_rgb')
-    catesr_valid = CATESR(subset='valid', images_dir='/home/ec2-user/gan/data/images_rgb',
-                          caches_dir='/home/ec2-user/gan/data/caches_rgb')
+    catesr_train = CATESR(subset='train', images_dir='/home/ec2-user/gans/data/images_rgb',
+                          caches_dir='/home/ec2-user/gans/data/caches_rgb')
+    catesr_valid = CATESR(subset='valid', images_dir='/home/ec2-user/gans/data/images_rgb',
+                          caches_dir='/home/ec2-user/gans/data/caches_rgb')
 
 
     train_ds = catesr_train.dataset(batch_size=16, random_transform=True, shuffle_buffer_size=500)
     valid_ds = catesr_valid.dataset(batch_size=1, random_transform=False, repeat_count=1)
 
-    # trainer = EdsrTrainer(model=edsr(scale=scale, num_res_blocks=depth), checkpoint_dir=f'.ckpt/edsr-{depth}-x{scale}')
-    #
-    # # Train EDSR model for 300,000 steps and evaluate model
-    # # every 1000 steps on the first 10 images of the DIV2K
-    # # validation set. Save a checkpoint only if evaluation
-    # # PSNR has improved.
-    # trainer.train(train_ds,
-    #               valid_ds.take(20),
-    #               steps=300000,
-    #               evaluate_every=1000,
-    #               save_best_only=True)
-    #
-    # # Restore from checkpoint with highest PSNR
-    # trainer.restore()
-    #
-    # # Evaluate model on full validation set
-    # psnrv = trainer.evaluate(valid_ds)
-    # print(f'PSNR = {psnrv.numpy():3f}')
-    #
-    # # Save weights to separate location (needed for demo)
-    # trainer.model.save_weights(weights_file)
+    generator_model = edsr(scale=scale, num_res_blocks=depth)
+    generator_model.load_weights(os.path.join(weights_dir, 'pretrained_weights-edsr-16-x4-fine-tuned.h5'))
+
+    trainer = EdsrTrainer(model=generator_model, checkpoint_dir=f'.ckpt/edsr-{depth}-x{scale}')
+
+    # Train EDSR model for 300,000 steps and evaluate model
+    # every 1000 steps on the first 10 images of the DIV2K
+    # validation set. Save a checkpoint only if evaluation
+    # PSNR has improved.
+    trainer.train(train_ds,
+                  valid_ds.take(20),
+                  steps=300000,
+                  evaluate_every=1000,
+                  save_best_only=True)
+
+    # Restore from checkpoint with highest PSNR
+    trainer.restore()
+
+    # Evaluate model on full validation set
+    psnrv = trainer.evaluate(valid_ds)
+    print(f'PSNR = {psnrv.numpy():3f}')
+
+    # Save weights to separate location (needed for demo)
+    trainer.model.save_weights(weights_file)
 
     # Create EDSR generator and init with pre-trained weights
     generator = edsr(scale=4, num_res_blocks=16)
@@ -60,7 +63,7 @@ if __name__ == "__main__":
 
     # Fine-tune EDSR model via SRGAN training.
     gan_trainer = SrganTrainer(generator=generator, discriminator=discriminator())
-    gan_trainer.train(train_ds, steps=50000)
+    gan_trainer.train(train_ds, steps=200000)
 
     new_weights_file = os.path.join(weights_dir, 'weights_fine_tuned_50000_steps.h5')
     generator.save_weights(new_weights_file)
